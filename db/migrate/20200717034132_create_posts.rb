6823295d8a4a2e7e50a5c2cd709a1cf804dd1714
SamHo <sam.ho@kdanmobile.com>
2020-07-17 14:53:54 +0800

    Update post with associating to user and comment

--- db/migrate/20200717034132_create_posts.rb
+++ db/migrate/20200717034132_create_posts.rb
@@ -1,8 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.string :title, null: false
+      t.string :content
       t.integer :rating
+      t.references :user, null: false, foreign_key:true
 
       t.timestamps
     end
