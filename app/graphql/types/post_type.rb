5b444eb5af70ec5e34cf12e515decf74b774c28d
SamHo <sam.ho@kdanmobile.com>
2020-07-17 12:01:26 +0800

    Remove comment in post type

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,6 +4,5 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :rating, Integer, null: true
-    field :comment, [Types::Comment], null: true, description: "This post's comments, or null if this post has comments disabled."
   end
 end
